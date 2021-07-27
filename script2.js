6831fd40661fa69f6d6f120917435dfe3de279d2
Iran55 <62675663+Iran55@users.noreply.github.com>
2021-07-27 15:15:51 -0300

    scirpt

---
+++ script2.js
@@ -0,0 +1,255 @@
+/* Denvolvido por Iran Felix Sampaio Ribeiro - Iran.ribeiro55@gmail.com - 27/07/2021 */
+
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("1") /*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 65){ /*LETRA A*/
+    const btn = document.querySelector("#1");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("2")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 66){/*LETRA B*/
+    const btn = document.querySelector("#2");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("3")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 67){/*LETRA C*/
+    const btn = document.querySelector("#3");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("4")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 68){/*LETRA D*/
+    const btn = document.querySelector("#4");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("5")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 69){/*LETRA E*/
+    const btn = document.querySelector("#5");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("6")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 70){/*LETRA F*/
+    const btn = document.querySelector("#6");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("7")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 71){/*LETRA G*/
+    const btn = document.querySelector("#7");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("8")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 72){/*LETRA H*/
+    const btn = document.querySelector("#8");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("9")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 73){/*LETRA I*/
+    const btn = document.querySelector("#9");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("10")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 74){/*LETRA J*/
+    const btn = document.querySelector("#10");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("11")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 75){/*LETRA K*/
+    const btn = document.querySelector("#11");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("12")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 76){/*LETRA L*/
+    const btn = document.querySelector("#12");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("13")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 77){/*LETRA M*/
+    const btn = document.querySelector("#13");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("14")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 78){/*LETRA N*/
+    const btn = document.querySelector("#14");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("15")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 79){/*LETRA O*/
+    const btn = document.querySelector("#15");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("16")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 80){/*LETRA P*/
+    const btn = document.querySelector("#16");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("17")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 81){/*LETRA Q*/
+    const btn = document.querySelector("#17");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("18")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 82){/*LETRA R*/
+    const btn = document.querySelector("#18");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("19")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 83){/*LETRA S*/
+    const btn = document.querySelector("#19");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("20")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 84){/*LETRA T*/
+    const btn = document.querySelector("#20");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("21")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 85){/*LETRA U*/
+    const btn = document.querySelector("#21");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
+
+document.addEventListener("keydown", function(event){
+
+    var obj=document.getElementById("22")/*AQUI COLOCA O ID*/
+    var tecla=event.keyCode;
+    if(tecla === 86){/*LETRA V*/
+    const btn = document.querySelector("#22");/*AQUI COLOCA O ID*/
+
+    btn.click();
+}
+
+});
